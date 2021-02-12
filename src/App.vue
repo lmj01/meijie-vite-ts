e3d095d198e0fb622a8dc468b87cef513d7438b7
lmj01 <lmj01@users.noreply.github.com>
2021-02-12 21:37:23 +0800

    add babylonjs library to test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,57 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <canvas id="idCanvas"></canvas>
+  </div>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import {
+  Engine, 
+  Scene, 
+  FreeCamera,
+  Vector3,
+  HemisphericLight,
+  Mesh,
+} from 'babylonjs'
+import cssExample from './css/example.module.css'
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted() {
+    this.initialBabylon();
+  },
+  methods: {
+    initialBabylon() {
+      let canvas = document.getElementById('idCanvas');
+      canvas.className = cssExample.red;
+      let engine = new Engine(canvas, true, {preserveDrawingBuffer:true, stencil:true});
+      let createScene = function() {
+        let scene = new Scene(engine);
+        let camera = new FreeCamera('camera1', new Vector3(0,5,-10), scene);
+        camera.setTarget(Vector3.Zero());
+        camera.attachControl(canvas, false);
+
+        let light = new HemisphericLight('light1', new Vector3(0,1,0), scene);
+        let sphere = Mesh.CreateSphere('sphere1', 16, 2, scene, false, Mesh.FRONTSIDE);
+        sphere.position.y = 1;
+        let ground = Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+        return scene;
+      }
+      let scene = createScene();
+      engine.runRenderLoop(function(){
+        scene.render();
+      });
+      window.addEventListener('resize', function(){
+        engine.resize();
+      })
+    }
   }
 })
 </script>
