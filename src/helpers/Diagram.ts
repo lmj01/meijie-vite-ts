0931bb1772cb85a590c493442ad97da575681195
lmj01 <lmj01@users.noreply.github.com>
2021-07-11 00:17:31 +0800

    update

--- src/helpers/Diagram.ts
+++ src/helpers/Diagram.ts
@@ -23,6 +23,12 @@ export function d3Tree(vWidth, vHeight, treeData) {
     ud.gTy = ud.mt
     ud.width = vWidth - ud.ml - ud.mr
     ud.height = vHeight - ud.mt - ud.mb
+    const rc = { // rectangle
+        width: 200,
+        height: 100,
+        titleWidth: 80,
+        titleHeight: 40,
+    }
 
     // declares a tree layout and assigns the size
     const treemap = d3.tree().size([ud.width, ud.width]);
@@ -36,10 +42,16 @@ export function d3Tree(vWidth, vHeight, treeData) {
     nodes.y0 = 0;
 
     // Normalize for fixed-depth.
-    nodes.each(function(node){ node.y = node.depth * 180; })
+    nodes.each(function(node){ 
+        node.y = node.depth * 180
+        // add margin for each node
+        node.x += node.depth * (rc.width + rc.titleWidth)
+        node.y += node.depth * (rc.height + rc.titleHeight) 
+    })
 
     const mouseMoved = (item) => {        
         const {sourceEvent, transform} = item
+        // console.log('-zoom-', sourceEvent, sourceEvent.type, transform)
         if (ud.oPosX < 0) ud.oPosX = sourceEvent.clientX
         if (ud.oPosY < 0) ud.oPosY = sourceEvent.clientY
         let offsetX = sourceEvent.clientX - ud.oPosX
@@ -52,12 +64,15 @@ export function d3Tree(vWidth, vHeight, treeData) {
         }
         ud.gTx += offsetX
         ud.gTy += offsetY
+
         ud.oPosX = sourceEvent.clientX
         ud.oPosY = sourceEvent.clientY
         ud.g.attr("transform", `translate(${ud.gTx},${ud.gTy})  scale(${ud.scale})`);
-        console.log('zoom', sourceEvent, offsetX, offsetY, ud)
+        // console.log('zoom', offsetX, offsetY, ud.gTx, ud.gTy, sourceEvent.clientX, sourceEvent.clientY)
     }
     const clickNode = (event, node) => {
+        const [[x0, y0], [x1, y1]] = d3.geoPath().bounds(node);
+        console.log('-clieckd-', x0, x1, y0, y1)
         console.log('-click node-', node)
         if (node.children) {
             node._children = node.children;
@@ -85,6 +100,21 @@ export function d3Tree(vWidth, vHeight, treeData) {
             .x(d => d.y)
             .y(d => d.x)
 
+    // d3的缩放时，是有一个缩放中心点的，就是鼠标所在位置，        
+    const zoom = d3.zoom()
+        .extent([0, 0,], [vWidth, vHeight])
+        .scaleExtent([1, 8])
+        .on('zoom', mouseMoved)
+        // .on('zoom', function({transform, sourceEvent}){
+        //     console.log('--', transform, sourceEvent.target, sourceEvent, this)
+        //     // ud.g.attr("transform", transform);
+        //     // ud.g.attr("stroke-width", 1 / transform.k);
+        //     // nodes.each(function(node){
+        //     //     console.log('-on zoom nodeaa-', node)
+        //     //     // node.attr("transform", d => `translate(${d.y},${d.x})`)
+        //     //  })
+        // })
+
     const updateAll = (source) => {
         if (!ud.g) return
         // adds the links between the nodes
@@ -96,25 +126,6 @@ export function d3Tree(vWidth, vHeight, treeData) {
             .style('stroke', d => d.data.level)
             .attr('d', d => `M ${d.y+200},${d.x} C ${(d.y + d.parent.y) / 2},${d.x} ${(d.y + d.parent.y) / 2},${d.parent.x} ${d.parent.y},${d.parent.x}`)
             // .attr('d', linkFunc)
-
-        // var link = ud.svg.selectAll("path.link")
-        //     .data(nodes.links(), d => d);
-
-        // // Enter any new links at the parent's previous position.
-        // link.enter().insert("path", "g")
-        //     .attr("class", "link")
-        //     .attr("d", linkFunc);
-
-        // // Transition links to their new position.
-        // link.transition()
-        //     .duration(ud.duration)
-        //     .attr("d", linkFunc);
-
-        // // Transition exiting nodes to the parent's new position.
-        // link.exit().transition()
-        //     .duration(ud.duration)
-        //     .attr("d", linkFunc)
-        //     .remove();
 
         // adds each node as a group
         const node = ud.g.selectAll('.node')
@@ -130,30 +141,44 @@ export function d3Tree(vWidth, vHeight, treeData) {
 
         bodyGroup.append('rect')
             .attr('class', 'body-box')
-            .attr('width', 200)
-            .attr('height', 80)
+            .attr('width', rc.width)
+            .attr('height', rc.height)
             .style("fill", d => d.children ? "lightsteelblue" : "yellow")
         
         bodyGroup.append('text')
+            .attr('transform', `translate(${rc.width/4},${rc.height*0.2})`)
             .attr('dy', '.35em')
-            .style('text-anchor', d => `${d.children ? 'end':'start'}`)
-            .text(d => d.data.name)
+            .text(d => '姓名'+ d.data.name)
             .style("fill-opacity", 0.5);
         
+        bodyGroup.append('text')
+            .attr('transform', `translate(${rc.width/4},${rc.height*0.4})`)
+            .attr('dy', '.35em')
+            .text(d => '电话' + d.data.name)
+            .style("fill-opacity", 0.5);
+        
+        bodyGroup.append('text')
+            .attr('transform', `translate(${rc.width/4},${rc.height*0.6})`)
+            .attr('dy', '.35em')
+            .text(d => '邮件' + d.data.name)
+            .style("fill-opacity", 0.5);
+        
+
         // title group
         const titleGroup = node.append('g')
             .attr('class', 'title-group')
-            .attr('transform', `translate(${-20},${-10})`)
+            .attr('transform', `translate(${-rc.titleWidth*0.5},${-rc.titleHeight*0.5})`)
 
         titleGroup.append('rect')
             .attr('class', 'title-box')
-            .attr('width', 100)
-            .attr('height', 40)
+            .attr('width', rc.titleWidth)
+            .attr('height', rc.titleHeight)
             .style("fill", d => d.children ? "lightsteelblue" : "yellow")
 
         titleGroup.append('text')
+            .attr('transform', `translate(${rc.titleWidth/6},${rc.titleHeight*0.5})`)
             .attr('dy', '.35em')
-            .text(d => d.data.name)
+            .text(d => '角色' + d.data.name)
             .style("fill-opacity", 0.5);
     }
     
@@ -162,25 +187,16 @@ export function d3Tree(vWidth, vHeight, treeData) {
         // appends a 'group' element to 'svg'
         // moves the 'group' element to the top left margin
         let elRoot = document.getElementById(elId)
-        elRoot.addEventListener('wheel', (event) => {
-
-        }, false)
         ud.svg = d3.select(`#${elId}`)
             .append('svg')
-                .attr('viewBox', [0, 0, vWidth, vHeight])
-                .attr('width', vWidth)
-                .attr('height', vHeight)
-        
+            .attr('viewBox', [0, 0, vWidth, vHeight])
+            .attr('width', vWidth)
+            .attr('height', vHeight)
         
         ud.g = ud.svg.append('g')
             .attr('transform', `translate(${ud.gTx},${ud.gTy}) scale(${ud.scale})`)
 
-        ud.svg.call(
-            d3.zoom()
-                .extent([0, 0,], [vWidth, vHeight])
-                .scaleExtent([1, 8])
-                .on('zoom', mouseMoved)
-        )
+        ud.svg.call(zoom)
 
         updateAll(nodes)        
     }
